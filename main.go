e503db776f2ee6a1bdc62cce96ac788e909fa53a
tingjeremy <tingjeremypriv@gmail.com>
2020-04-26 18:56:34 +0800

    update all source code

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "log"
+import (
+	"log"
+	"fmt"
+	"net/http"
+	"blog/routers"
+	"blog/pkg/setting"
+)
 
 func main() {
 	log.Printf("Nunu's Blog Version 2.0 Start! \n")
+
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler: router,
+		ReadTimeout: setting.ReadTimeOut,
+		WriteTimeout: setting.WriteTimeOut,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 
 }
